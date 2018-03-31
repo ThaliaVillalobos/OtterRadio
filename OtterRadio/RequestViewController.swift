dda5829fe5ce6c8fa5a4c9a497ff9143378896a0
thaliavillalobos <thaliavillalobos@student.hartnell.edu>
2018-03-31 02:30:49 -0700

    request page is on saved in our database

--- OtterRadio/RequestViewController.swift
+++ OtterRadio/RequestViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import Parse
 
 class RequestViewController: UIViewController {
 
+    @IBOutlet weak var artistName: UITextField!
+    @IBOutlet weak var songTitle: UITextField!
+    @IBOutlet weak var shoutOut: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,5 +23,29 @@ class RequestViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    @IBAction func onSubmit(_ sender: AnyObject) {
+        let requestSong = PFObject(className: "Request")
+        
+        requestSong["artist"] = artistName.text ?? ""
+        requestSong["song"] = songTitle.text ?? ""
+        requestSong["shoutOut"] = shoutOut.text ?? ""
+        requestSong["user"] = PFUser.current()
+        
+        
+        requestSong.saveInBackground { (success, error) in
+            if success {
+                print("The request is saved!")
+                self.artistName.text = ""
+                self.songTitle.text = ""
+                self.shoutOut.text = ""
+                
+            } else if let error = error {
+                print("Problem saving message: \(error.localizedDescription)")
+            }
+        }
+        
+        performSegue(withIdentifier: "backHome", sender: nil)
+        
+    }
 
 }
