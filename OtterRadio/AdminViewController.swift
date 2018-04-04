14a4b6fd33b3ff2f5947bba1ba6b81e2f5f596b2
Thalia Villalobos <vill9422@m506-246-25987.local>
2018-04-03 17:32:46 -0700

    seach bar now display the name of user

--- OtterRadio/AdminViewController.swift
+++ OtterRadio/AdminViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 import Parse
 
-class AdminViewController: UIViewController, UITableViewDataSource {
+class AdminViewController: UIViewController, UITableViewDataSource,UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var users: [PFObject] = []
-    
+    var filteredData: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchName()
         tableView.dataSource = self
+        searchBar.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,19 +35,41 @@ class AdminViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as! UserCell
-        
-        //Todo: dispaly username
+
         let user = users[indexPath.row]
         cell.userNameLabel.text = user["username"] as? String
         
         return cell
     }
     
+    
+    
+    
+//Todo: Fix the checkmarks
+    //Adding Checkmarks on usernames
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+//        tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.checkmark
+//    }
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//        
+//        if let cell = tableView.cellForRow(at: indexPath as IndexPath) {
+//            if cell.accessoryType == .checkmark{
+//                cell.accessoryType = .none
+//            }
+//            else{
+//                cell.accessoryType = .checkmark
+//            }
+//        }
+//        
+//    }
+    
+    
+    
     //Fetching Names
     func fetchName() {
         let query = PFQuery(className: "_User")
         query.includeKey("username")
-        
         
         query.findObjectsInBackground { ( users: [PFObject]?, error: Error?) in
             if let userName = users {
@@ -54,6 +78,21 @@ class AdminViewController: UIViewController, UITableViewDataSource {
             } else {
                 print("Error receiving the messages")
             }
+        }
+    }
+    
+    
+    //Todo: fix name to match the searchText
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        
+        let user = PFQuery(className: "_User")
+        
+        if searchText != nil{
+            user.whereKey("username", contains: searchText)
+        }
+        user.findObjectsInBackground { (results, error) -> Void in
+            self.users = (results)!
+            self.tableView.reloadData()
         }
     }
 
