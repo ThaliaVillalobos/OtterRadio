17083f9a8a75843e1273b142416914dddc124753
aceLeVillegas <sarahvillegas95@gmail.com>
2018-04-18 12:01:36 -0700

    Saving spot where stopped

--- OtterRadio/LiveChatViewController.swift
+++ OtterRadio/LiveChatViewController.swift
@@ -15,6 +15,8 @@ class LiveChatViewController: UIViewController,UITableViewDataSource  {
     @IBOutlet weak var messageField: UITextField!
     var messages: [PFObject] = []
     
+    var userCols: [UIColor] = [ UIColor(hue: 46.0 , saturation: 59.0 , brightness: 100.0, alpha: 1.0), UIColor(hue: 115.0, saturation: 40.0, brightness: 77.0, alpha: 1.0) , UIColor.white ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -64,6 +66,9 @@ class LiveChatViewController: UIViewController,UITableViewDataSource  {
         if let user = mess["user"] as? PFUser {
             // User found! update username label with username
             cell.name.text = user.username
+            let iColor = indexPath.row % 3
+            print("\(iColor)\n")
+            cell.name.textColor = userCols[iColor]
         } else {
             // No user found, set default username
             cell.name.text = "🤖"
