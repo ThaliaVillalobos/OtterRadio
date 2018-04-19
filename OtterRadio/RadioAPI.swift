a519ef83666287f916f20b51b11f330933d1611b
Mario Martinez <mart1041@m506-246-25987.local>
2018-04-18 17:43:06 -0700

    Fixed a bug with the Playing info center. It would display the play button when it was already playing.

--- OtterRadio/RadioAPI.swift
+++ OtterRadio/RadioAPI.swift
@@ -19,7 +19,14 @@ class RadioAPI {
     var isPlaying:Bool?
 
     init() {
+        
+        
         let url = URL(string: radioURL)
+        if AVAsset(url: url!).isPlayable {
+            print("The link is working")
+        }else{
+            print("Error: Radio link is down")
+        }
         playerItem = AVPlayerItem(url: url!)
         player = AVPlayer(playerItem: playerItem)
         
@@ -45,7 +52,7 @@ class RadioAPI {
     
     private func setupCommandCenter() {
         MPNowPlayingInfoCenter.default().nowPlayingInfo = [MPMediaItemPropertyTitle: "Otter Radio"]
-        
+        MPNowPlayingInfoCenter.default().nowPlayingInfo?.updateValue((player?.rate)!, forKey: MPNowPlayingInfoPropertyPlaybackRate)
         let commandCenter = MPRemoteCommandCenter.shared()
         commandCenter.playCommand.isEnabled = true
         commandCenter.pauseCommand.isEnabled = true
@@ -61,6 +68,11 @@ class RadioAPI {
     
     func initPlayer() {
         let url = URL(string: radioURL)
+        if AVAsset(url: url!).isPlayable {
+            print("The link is working")
+        }else{
+            print("Error: Radio link is down")
+        }
         playerItem = AVPlayerItem(url: url!)
         player = AVPlayer(playerItem: playerItem)
 
@@ -86,13 +98,17 @@ class RadioAPI {
             player!.play()
         }
         isPlaying = true
+        MPNowPlayingInfoCenter.default().nowPlayingInfo?.updateValue((player?.rate)!, forKey: MPNowPlayingInfoPropertyPlaybackRate)
+
     }
     
     func stopRadio() {
         if player != nil {
             player!.pause()
+            MPNowPlayingInfoCenter.default().nowPlayingInfo?.updateValue((player?.rate)!, forKey: MPNowPlayingInfoPropertyPlaybackRate)
             player = nil
         }
         isPlaying = false
+
     }
 }
